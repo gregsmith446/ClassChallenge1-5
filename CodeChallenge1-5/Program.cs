d8f1d7666b8a91199d3ff92ba6713b602d0ad9ea
Greg Smith <gregsmith446@gmail.com>
2019-01-05 21:02:13 -0500

    completed challenge

--- CodeChallenge1-5/Program.cs
+++ CodeChallenge1-5/Program.cs
@@ -10,6 +10,37 @@ namespace CodeChallenge1_5
     {
         static void Main(string[] args)
         {
+            var callClass = new CompareStrings();
+
+            int result = callClass.CompareString("this", "bear", 4);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    class CompareStrings
+    {
+        public int CompareString(string s1, string s2, int sLength)
+        {
+            // convert both strings to char array
+            char[] charArr1 = s1.ToCharArray();
+            char[] charArr2 = s2.ToCharArray();
+
+            // initialize an array of ints that will hold the total differences between s1 & s2
+            // List<int> charTotal = new List<int>();
+            int total = 0;
+
+            // loop through both chars
+            for (int i = 0; i < sLength; i++)
+            {
+                int diff = charArr2[i] - charArr1[i];
+                Console.WriteLine("The difference between the 2 chars is {0}.", diff);
+                total = Math.Abs(diff) + total;
+                // total += diff // shortcut version 
+            }
+
+            // Console.WriteLine(total);
+            return total;
         }
     }
 }
